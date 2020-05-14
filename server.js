c3aaabe83fe1027e17919f540d42733aaf108e5b
SMoore3773 <Smooresculpture@gmail.com>
2020-05-13 20:32:01 -0400

    mvp

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
-
+const helmet = require('helmet');
+const morgan = require('morgan');
+const userRouter = require('./users/userRouter.js');
 const server = express();
+
+server.use(express.json());
+server.use(helmet());
+server.use(logger);
+
+server.use('/api/users', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +16,15 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(`${req.method} Request to ${req.url} at [${new Date().toISOString()}]  `)
+  next();
+}
+function errorHandler(error, req, res, next) {
+  console.log('error:', error.message);
+  const code = error.status || error.statusCode || 400;
+  res.status(code).json(error);
+}
+server.use(errorHandler);
 
 module.exports = server;
