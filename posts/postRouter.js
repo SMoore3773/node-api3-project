b26170c92572be3bf0f6eeaf79c58046cf98078e
SMoore3773 <Smooresculpture@gmail.com>
2020-05-14 21:47:30 -0400

    stretch for post router completed

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -2,26 +2,81 @@ const express = require('express');
 
 const router = express.Router();
 
+const postDB = require('./postDb');
+
 router.get('/', (req, res) => {
   // do your magic!
+  postDB.get()
+  .then(postList =>{
+    res.status(200).json(postList)
+  })
+  .catch(err=>{
+    res.status(500).json({message:'error in getting posts'})
+  })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id',validatePostId, (req, res) => {
   // do your magic!
+  postDB.getById(req.params.id)
+  .then(post =>{
+    res.status(200).json(post)
+  })
+  .catch(err =>{
+    res.status(500).json({message:`error in getting post with id of ${req.params.id}`})
+  }
+  )
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id',validatePostId, (req, res) => {
   // do your magic!
+  postDB.remove(req.params.id)
+  .then(post =>{
+    res.status(200).json({message: "the post has been deleted"})
+  })
+  .catch(err=>{
+    res.status(500).json({message: 'there was an error in deleting the post'})
+  })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',validatePostId, validatePost, (req, res) => {
   // do your magic!
+  const {id} = req.params;
+  let changes = req.body;
+  postDB.update(id,changes)
+  .then(post =>{
+    res.status(201).json(changes)
+  })
+  .catch(err =>{
+    res.status(500).json({message:"error in updating changes to the post"})
+  })
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
   // do your magic!
+  const {id} = req.params;
+  postDB.getById(id)
+  .then(post =>{
+    if(post){
+      req.post = post;
+      next();
+    }else{
+      res.status(400).json({message: "invalid post ID"})
+    }
+  })
+  .catch(err=>{
+    res.status(500).json({message:"error",err})
+  })
 }
-
+function validatePost(req, res, next) {
+  // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: "missing post data" })
+  } else if (!req.body.text) {
+    res.status(400).json({ message: "missing required text field" })
+  } else {
+    next();
+  }
+}
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 const server = express();
 
 server.use(express.json());
@@ -9,6 +10,7 @@ server.use(helmet());
 server.use(logger);
 
 server.use('/api/users', userRouter);
+server.use('/api/posts',postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
